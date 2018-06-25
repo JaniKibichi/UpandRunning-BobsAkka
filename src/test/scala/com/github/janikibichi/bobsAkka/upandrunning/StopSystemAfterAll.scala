8f11879c96c33f68fd7f954315967482149afc10
janikibichi <janikibichi@gmail.com>
2018-06-25 19:19:45 +0300

    update with testing

---
+++ src/test/scala/com/github/janikibichi/bobsAkka/upandrunning/StopSystemAfterAll.scala
@@ -0,0 +1,12 @@
+package com.github.janikibichi.bobsAkka.upandrunning
+
+import akka.testkit.TestKit
+import org.scalatest.{BeforeAndAfterAll, Suite}
+
+trait StopSystemAfterAll extends BeforeAndAfterAll {
+  this: TestKit with Suite =>
+  override protected def afterAll(): Unit = {
+    super.afterAll()
+    system.terminate()
+  }
+}
